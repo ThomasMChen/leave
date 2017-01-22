934c862faeab7b158cd68c334d43a91f7f2edca5
Thomas Chen <thomas99k@gmail.com>
2017-01-21 23:51:06 -0500

    Add SettingsActivity Skeleton
    Implemented SettingsActivity skeleton alongside MainActivity Intent for data sharing.

--- app/src/main/java/org/thomaschen/leave/MainActivity.java
+++ app/src/main/java/org/thomaschen/leave/MainActivity.java
@@ -1,7 +1,18 @@
 package org.thomaschen.leave;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button settingsBtn = (Button) findViewById(R.id.settingBtn);
+        ImageButton addBtn = (ImageButton) findViewById(R.id.addBtn);
+
+        final Switch switchBtn = (Switch) findViewById(R.id.switch1);
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivityForResult(i, 1);
+            }
+        } );
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        switchBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                ConstraintLayout alarm = (ConstraintLayout) switchBtn.getParent();
+
+                if (!isChecked) {
+                    for (int i = 0; i < alarm.getChildCount(); i++) {
+                        if (alarm.getChildAt(i) instanceof TextView) {
+                            ((TextView) alarm.getChildAt(i)).setTextColor(getResources().getColor(R.color.subduedTextColor));
+                        }
+                    }
+
+                } else {
+                    for (int i = 0; i < alarm.getChildCount(); i++) {
+                        if (alarm.getChildAt(i) instanceof TextView) {
+                            ((TextView) alarm.getChildAt(i)).setTextColor(getResources().getColor(R.color.white));
+                        }
+                    }
+                }
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/org/thomaschen/leave/SettingsActivity.java
@@ -0,0 +1,24 @@
+package org.thomaschen.leave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        ImageButton backBtn = (ImageButton) findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
